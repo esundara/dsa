77477f86b1e1e4359b49057e5ca39278cdc83027
esundara <esundara@yahoo.com>
2025-03-26 23:06:38 -0400

    Pyramid

---
+++ src/others/ASCIIDollarPyramid.java
@@ -0,0 +1,39 @@
+package others;
+
+/**
+ * Building an ASCII Pyramid With Stars based on input number of rows
+ * So for example if number of rows = 5, then print
+ * $
+ * $$$
+ * $$$$$
+ * $$$$$$$
+ * $$$$$$$$$
+ */
+public class ASCIIDollarPyramid {
+
+    public static void main(String[] args) {
+        generate(5);
+    }
+
+    public static void generate(int numberOfRows) {
+        int position = numberOfRows - 1;
+        int stars = 1;
+        for (int i = 0; i < numberOfRows; i++) {
+            String spaces = " ".repeat(position);
+            if (position > 0) {
+                position--;
+            }
+
+            StringBuilder str = new StringBuilder();
+            for (int j = 0; j < stars; j++) {
+                str.append("$");
+            }
+            stars = stars + 2;
+            System.out.println(spaces + str);
+
+        }
+
+    }
+
+
+}
